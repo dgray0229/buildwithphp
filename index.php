87a5a9b27cb77675b0325ed7738b5cf945bb9fec
Devin G <devin.gray92@gmail.com>
2016-03-26 23:13:51 -0700

    progress with twig config

--- index.php
+++ index.php
@@ -1,26 +1,57 @@
 <?php
 
+require 'vendor/autoload.php';
+
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require 'vendor/autoload.php';
+
 date_default_timezone_set('America/Los_angeles');
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-
 // $log = new Logger('name');
 // $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Logger::WARNING));
 // $log->addWarning('Zoinks');
 
-$app = new \Slim\App(["settings" => $config]);
-$app->get('/', function(){
-  echo 'Hello, this is the home page.';
-});
+// Create and configure Slim app
+$configuration = [
+  'settings' => [
+      'displayErrorDetails' => true,
+  ],
+];
+// Get container
+$container = $app->getContainer();
 
-$app->get('/contact', function(){
-  echo 'Feel free to contact us.';
-});
+// Register componet on container
+$container['view'] = function ($container) {
+  $view = new \Slim\Views\Twig('templates', [
+    'cache' => false
+  ]);
+  $view->addExtension(new \Slim\Views\TwigExtension(
+    $container['router'],
+    $container['request']->getUri()
+  ));
 
+  return $view;
+};
+
+// Creat App
+$app = new \Slim\App($container);
+
+// Render Twig template in route
+$app->get('/', function ($request, $response, $args) {
+  return $this->view->render($response, 'index.html', [
+    'name' => $args['name']
+  ]);
+})->setName('index');
+
+$app->get('/contact', function ($request, $response, $args) {
+  return $this->view->render($response, 'contact.html', [
+    'name' => $args['name']
+  ]);
+})->setName('contact');
+
+// Run app
 $app->run();
